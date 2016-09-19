510f4272f00e1395669c1af3964e1ec99949b989
maderkse <maderkse@gmail.com>
2016-09-19 17:18:05 +0200

    Filetés added

---
+++ src/FileTest.java
@@ -0,0 +1,8 @@
+public class FileTest
+{
+	public void helloWorld()
+	{
+		System.out.println("Hello World, I am " + this.getClass().getName() + "!");
+		System.out.println("----------------------------------");
+	}
+}
